41f4721fc5cc199ce4e17edf82692a3181be8db7
Simon Bedoya <simon.bedoya@rappi.com>
2021-04-10 18:02:36 -0500

    Creacion de clases de labores, tipos de labores, productos de control y tipos de productos de control

--- viveros/webapp/models/__init__.py
+++ viveros/webapp/models/__init__.py
@@ -1,3 +1,7 @@
 from webapp.models.ciudades import Ciudades
+from webapp.models.labores import Labores
+from webapp.models.tipos_de_labores import Tiposlabores
+from webapp.models.productos_de_control import Productoscontrol
+from webapp.models.tipos_productos_control import Tiposproductoscontrol
 
-__all__=['Ciudades']
+__all__=['Ciudades', 'Labores', 'Tiposlabores' , 'Productoscontrol' , 'Tiposproductoscontrol']
