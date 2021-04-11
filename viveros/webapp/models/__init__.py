c9df4bfb0c47421d1e7ceddc0e4e6b787a977151
danielvillamorales <daniel.villa@utp.edu.co>
2021-04-10 20:10:50 -0500

    creacion de primer merge

--- viveros/webapp/models/__init__.py
+++ viveros/webapp/models/__init__.py
@@ -1,16 +1,11 @@
 from webapp.models.ciudades import Ciudades
-<<<<<<< HEAD
 from webapp.models.productores import Productores
 from webapp.models.productores_viveros import Productores_viveros
 from webapp.models.viveros import Viveros
-
-__all__=['Ciudades', 'Productores', 'Viveros','Productores_viveros']
-
-=======
-from webapp.models.labores import Labores
 from webapp.models.tipos_de_labores import Tiposlabores
 from webapp.models.productos_de_control import Productoscontrol
 from webapp.models.tipos_productos_control import Tiposproductoscontrol
+from webapp.models.labores import Labores
 
-__all__=['Ciudades', 'Labores', 'Tiposlabores' , 'Productoscontrol' , 'Tiposproductoscontrol']
->>>>>>> hu_2
+__all__=['Ciudades', 'Productores', 'Viveros','Productores_viveros',, 'Labores', 'Tiposlabores' , 'Productoscontrol' , 'Tiposproductoscontrol']
+
